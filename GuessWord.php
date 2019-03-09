b5247bc00f3f249d40080e467622649f4903e6d1
spatel223 <44443677+spatel223@users.noreply.github.com>
2019-03-09 16:24:41 -0500

    Update GuessWord.php

--- GuessWord.php
+++ GuessWord.php
@@ -24,7 +24,7 @@ h1 {
 <h1> Guess The Word: Spongebob Edition! :) <h1>
 
 <?php
-
+import 'leaderboard.php' ;
 
 $letters = array('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
 
