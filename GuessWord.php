aa840ce69d1e3a432e9f9cf72b1f415a34256a38
Nidhi Parmar <36490835+nparmar3@users.noreply.github.com>
2019-07-30 20:10:26 -0400

    Update GuessWord.php

--- GuessWord.php
+++ GuessWord.php
@@ -8,7 +8,6 @@
 
 <h1> Guess The Word: Spongebob Edition! :) </h1>
 
-
 <?php
 
 $letters = array('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
